4bc35adab91e7f13937cd828935792ab5f3807d9
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-03-27 13:13:59 -0400

    COOKIE CLICKER

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Junyoung
+ * Created on: Mar 2026
+ * This program 
 */
 
-basic.showString('Hello, World!')
+// variables 
+let cookies: number = 0
+
+// start up
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// press A
+input.onButtonPressed(Button.A, function() {
+    cookies += 1
+    basic.showString(cookies.toString())
+})
+
+// press B
+input.onButtonPressed(Button.B, function() {
+    cookies = 0
+    basic.showString(cookies.toString())
+})
